1d18d81d159e2f074706c32e8d2a447c19856e52
meo20200530 <942769811@qq.com>
2022-07-21 09:47:43 +0800

    第一次更新

---
+++ src/test/java/com/oyc/springbootwebdemo/SpringBootWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oyc.springbootwebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
